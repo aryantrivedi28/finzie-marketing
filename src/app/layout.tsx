e0518edd652b10a905809cfa74895bfd67f9a6f9
kunal <something>
2026-04-15 13:18:05 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,12 +47,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/exeicon.png', sizes: 'any' },
+      { url: '/exeicon.png', sizes: '16x16', type: 'image/png' },
+      { url: '/exeicon.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/exeicon.png', sizes: '180x180', type: 'image/png' },
     ],
     other: [
       {
